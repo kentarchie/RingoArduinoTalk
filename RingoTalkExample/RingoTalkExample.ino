af811d020be3b446e1d7dbf5562862d0089dbcea
kent archie <kentarchie@gmail.com>
2017-10-26 07:34:33 +0100

    Basic light tracking working

--- RingoTalkExample/RingoTalkExample.ino
+++ RingoTalkExample/RingoTalkExample.ino
@@ -1,5 +1,6 @@
 /*
 
+The base code of this sketch is from:
 Ringo Robot
 Ringo_Base_Sketch_02
 Version 2.0 8/2015
@@ -16,6 +17,7 @@ https://creativecommons.org/licenses/by-sa/2.0/
 Visit http://www.plumgeek.com for Ringo information.
 Visit http://www.arduino.cc to learn about the Arduino.
 
+The code that does all the functions is written by Kent Archie
 */
 
 #include "RingoHardware.h"
@@ -39,14 +41,16 @@ int InitialAverageLight = 0;
 int TooDarkValue = 0; 
 int AreHeadLightsOn = FALSE; 
 
-void GetLightSensors()
+int LoopCounter = 0;   // used in log printing
+
+void GetRearLightSensors()
 {
    LightSensorValue = analogRead(LightSense_Rear); // read Rear light sensor
-} // GetLightSensors
+} // GetRearLightSensors
 
 void GetInitialLightValue() 
 {
-   Serial.println("Getting Light Average");
+   printString("Getting Light Average");
    digitalWrite(Source_Select, HIGH); //This selects the top light sensors
    for (int i = 0 ; i < NUM_LIGHT_READINGS; ++i) {
       InitialAverageLight += analogRead(LightSense_Rear);
@@ -55,10 +59,10 @@ void GetInitialLightValue()
    InitialAverageLight = InitialAverageLight / 4;
    TooDarkValue = InitialAverageLight * .20;
    printValue("Base Light Level = ", InitialAverageLight);
-   printValue("too Dark Light Level = ", TooDarkValue);
+   printValue("TooDarkValue = ", TooDarkValue);
 } // GetInitialLightValue
 
-void LightsOn()
+void LightsOnLoop()
 {
   for (int pixel = 0 ; pixel < NUM_PIXELS; ++pixel) {
       if((AreHeadLightsOn && ((pixel == EYE_LEFT) || (pixel == EYE_RIGHT))))
@@ -68,7 +72,7 @@ void LightsOn()
       OffPixel(pixel);
       delay(PIXEL_DELAY);
   }
-} // LightsOn
+} // LightsOnLoop
 
 void HeadLightsOn(int averageLightValue) 
 {
@@ -76,18 +80,66 @@ void HeadLightsOn(int averageLightValue)
    if((averageLightValue < InitialAverageLight) && 
       ((InitialAverageLight - averageLightValue) > TooDarkValue))
       {
-         Serial.println("Too Dark");
-         OnEyes(100, 100, 100);
+         printString("Too Dark");
+         OnEyes(100, 100, 100); // turn on front lights
          AreHeadLightsOn = TRUE;
          
       }
       else {
-         Serial.println("Not Dark");
-         OffEyes();
+         printString("Not Dark");
+         //OffEyes();
+         SetPixelRGB( 4, 0, 0, 0);
+         SetPixelRGB( 5, 0, 0, 0);
          AreHeadLightsOn = FALSE;
-         GetLightSensors();
+         GetRearLightSensors();
       }
 } // HeadLightsOn
+
+// check the front left and right light sensors
+// if the values are more than 20% higher than the base value
+// start the motor on that side to move the Ringo in that direction
+void FollowLight() 
+{
+   int leftSensorValue = 0, rightSensorValue = 0;
+   //OffEyes(); // need front lights off to take readings
+   SetPixelRGB( 4, 0, 0, 0);
+   SetPixelRGB( 5, 0, 0, 0);
+   Serial.println("Eyes Off");
+
+   leftSensorValue  = analogRead(LightSense_Left);
+   printValue("Left Sensor = ", leftSensorValue);
+   rightSensorValue = analogRead(LightSense_Right);
+   printValue("FollowLight: Right Sensor = ", rightSensorValue);
+
+   if(AreHeadLightsOn) {
+      GetRearLightSensors();
+      HeadLightsOn(LightSensorValue);
+   }
+   printString("FollowLight: head light check reset");
+
+   if((leftSensorValue > InitialAverageLight) && 
+      ((leftSensorValue - InitialAverageLight ) > TooDarkValue))
+      {
+         printString("Turning Left 90 degrees");
+         RotateAccurate(-90, 1500);
+         SwitchMotorsToSerial(); //Call before using Serial.print functions as motors & serial share a line
+         
+         printString("Left move complete");
+         PlayLeftTune();
+         printString("Left tune complete");
+      }
+   if((rightSensorValue > InitialAverageLight) && 
+      ((rightSensorValue - InitialAverageLight ) > TooDarkValue))
+      {
+         printString("Turning Right 90 degrees");
+         RotateAccurate(90, 1500);
+         SwitchMotorsToSerial(); //Call before using Serial.print functions as motors & serial share a line
+         
+         printString("Right move complete");
+         PlayRightTune();
+         printString("Right tune complete");
+      }
+} // FollowLight
 
 void PlayRightTune()
 {
@@ -98,12 +150,22 @@ void PlayRightTune()
 void PlayLeftTune()
 {
    PlaySweep(4000, 1000, 330);
-   OffChirp();
+   //OffChirp();
 } // PlayLeftTune
 
 void printValue(char * label,int value) {
+   //SwitchMotorsToSerial(); //Call before using Serial.print functions as motors & serial share a line
+   Serial.print(LoopCounter);
+   Serial.print(": ");
    Serial.print(label);
    Serial.println(value);
+} // printValue
+
+void printString(char * label) {
+   //SwitchMotorsToSerial(); //Call before using Serial.print functions as motors & serial share a line
+   Serial.print(LoopCounter);
+   Serial.print(": ");
+   Serial.println(label);
 } // printValue
 
 void setup()
@@ -114,29 +176,29 @@ void setup()
    HardwareBegin();        //initialize Ringo's brain to work with his circuitry
    SwitchButtonToPixels(); //set shared line to control NeoPixel lights
    PlayStartChirp();       //Play startup chirp and blink eyes
-   SwitchMotorsToSerial(); //Call before using Serial.print functions as motors & serial share a line
    RestartTimer();
+   delay(1000);
+   NavigationBegin();
 
    GetInitialLightValue();
 
-  Serial.println("Setup Done");
+   printString("Setup Done");
 } //setup
 
 void loop()
 { 
-   delay(LIGHT_SENSE_DELAY);
+   LoopCounter++;
+  // delay(LIGHT_SENSE_DELAY);
 
-   //PlayLeftTune();
-   //PlayRightTune();
-
-   LightsOn();
+   LightsOnLoop();
 
    digitalWrite(Source_Select, HIGH); //This selects the top light sensors
-   GetLightSensors();
+   GetRearLightSensors();
 
-   printValue("Light Sensor = ", LightSensorValue);
+   printValue("Rear Light Sensor = ", LightSensorValue);
 
    HeadLightsOn(LightSensorValue);
+   FollowLight();
    
-   Serial.println("Loop Done");
+   printString("Loop Done");
 } // loop
